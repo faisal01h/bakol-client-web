5e6494c6d8f96632f1caaadd59a768cb0080827d
faisal01h <faisal01h@gmail.com>
2023-11-08 09:52:06 +0700

    QRIS is now functional

--- src/pages/checkout/[invoice].tsx
+++ src/pages/checkout/[invoice].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
 
 export default function ProductSku() {
     let params = useParams();
@@ -20,6 +21,8 @@ export default function ProductSku() {
     ]);
     const [ selectedMethod, setSelectedMethod ] = useState<any>();
     const [ data, setData ] = useState<any>();
+    const [ showQr, setShowQr ] = useState<boolean>(false);
+    const [ paymentToken, setPaymentToken ] = useState<string|undefined>();
 
     useEffect(() => {
         if(params && params.invoice) {
@@ -27,6 +30,13 @@ export default function ProductSku() {
             .then((e) => {
                 console.log('trx', e.data.transaction);
                 setData(e.data.transaction);
+
+                if(e.data.transaction.remarks !== '' && e.data.transaction.remarks !== '-') {
+                    setPaymentToken(e.data.transaction.remarks);
+                    if(e.data.transaction.payment_method === 'xendit.qris') {
+                        setShowQr(true);
+                    }
+                }
             })
         }
     }, [params])
@@ -42,6 +52,8 @@ export default function ProductSku() {
         createPayment(data.invoice, selectedMethod)
         .then((e) => {
             console.log(e)
+            setPaymentToken(e.payment.qr_string)
+            setShowQr(true)
         })
     }
 
@@ -73,31 +85,44 @@ export default function ProductSku() {
                         <label htmlFor="voucher">Kode Voucher</label>
                         <input type="text" id="voucher" className="border border-gray-300 text-lg rounded-md px-3 py-1 font-mono w-72 text-center" />
                     </div> */}
-                    <div>
+                    {
+                        !paymentToken ?
                         <div>
-                            <h3 className="font-semibold text-lg">Metode Pembayaran</h3>
+                            <div>
+                                <h3 className="font-semibold text-lg">Metode Pembayaran</h3>
+                            </div>
+                            <div className="flex flex-wrap gap-3">
+                                {
+                                    availableMethods.map((e:any, i:number) => {
+                                        return (
+                                            <div key={i} onClick={_=>setSelectedMethod(e.name)}>
+                                                <label htmlFor={e.name}>
+                                                    <div className={`h-32 w-32 flex flex-col items-center cursor-pointer justify-center hover:shadow ${selectedMethod === e.name ? "bg-emerald-600 text-white" : ""} rounded-md border transition-all`}>
+                                                        {e.display}
+                                                    </div>
+                                                </label>
+                                                <input type="radio" id={e.name} className="hide" />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div>
+                                <button onClick={pay}>Bayar</button>
+                                <button onClick={cancel}>Batal</button>
+                            </div>
                         </div>
-                        <div className="flex flex-wrap gap-3">
-                            {
-                                availableMethods.map((e:any, i:number) => {
-                                    return (
-                                        <div key={i} onClick={_=>setSelectedMethod(e.name)}>
-                                            <label htmlFor={e.name}>
-                                                <div className={`h-32 w-32 flex flex-col items-center cursor-pointer justify-center hover:shadow ${selectedMethod === e.name ? "bg-emerald-600 text-white" : ""} rounded-md border transition-all`}>
-                                                    {e.display}
-                                                </div>
-                                            </label>
-                                            <input type="radio" id={e.name} className="hide" />
-                                        </div>
-                                    )
-                                })
-                            }
+                        : showQr ?
+                        <div className="flex flex-col items-center gap-5">
+                            <span>Scan QR code QRIS berikut untuk melakukan pembayaran</span>
+                            <QRCode
+                                size={256}
+                                value={paymentToken}
+                                viewBox={`0 0 256 256`}
+                            />
                         </div>
-                        <div>
-                            <button onClick={pay}>Bayar</button>
-                            <button onClick={cancel}>Batal</button>
-                        </div>
-                    </div>
+                        : ""
+                    }
                 </div>
             </div>
         </StoreLayout>
